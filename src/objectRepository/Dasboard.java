150fdf3a86ae592f890f77955a55fa79c4270030
architas <architas@RSPC1195.rssoftware.com>
2017-09-08 10:45:56 +0530

    Dashboard chnage

--- src/objectRepository/Dasboard.java
+++ src/objectRepository/Dasboard.java
@@ -22,4 +22,6 @@ public class Dasboard {
 	public static String helthInfo=".//*[@id='page-wrapper']/div/div/div/div[5]/div/div[1]/label";
 	public static String logout=".//*[@id='rmenu']/div/div[1]/div/a/span";
 	
+	public static String header1=".//*[@id='wrapper']/nav/div/div[2]/h1";
+	
 }
