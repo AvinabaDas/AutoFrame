b7280342b1eb5c32733ec8e301cdc3424ff28ef3
architas <architas@RSPC1195.rssoftware.com>
2017-09-08 11:27:19 +0530

    UserMgmt-object creation

---
+++ src/objectRepository/RegisterUser.java
@@ -0,0 +1,48 @@
+package objectRepository;
+
+
+/*
+ * Locators displayed on Login Page
+ * 
+ * 
+ */
+
+
+public class RegisterUser {
+
+	/*----------------------------------------------------------------------------------------------------------*/
+	/*----------------------------------------------ID----------------------------------------------------------*/
+	/*----------------------------------------------------------------------------------------------------------*/
+	
+	//BBCU or BBPOU type selection 
+	public static String userTypeOu="userTypeOu";
+	public static String userTypeCu="userTypeCu";
+	
+	//Availibility check
+	
+	public static String outext="outext";
+	public static String oulist="oulist";
+	public static String loginId="loginId";
+	public static String checkAvailibilitybtn="loaderIcon";
+	public static String loadedErrorResult="loadedErrorResult";
+	public static String loadedSuccessResult="loadedSuccessResult";
+	
+	//Data entry 
+	public static String empId="empId";
+	public static String userName="userName";
+	
+	public static String lastName="lastName";
+	public static String emailId="emailId";
+	public static String mobileNo="mobileNo";
+	public static String departmntlst="departmntlst";
+	public static String desglst="desglst";
+	public static String admnlst="admnlst";
+	
+		
+	/*----------------------------------------------------------------------------------------------------------*/
+	/*----------------------------------------------XPATH-------------------------------------------------------*/
+	/*----------------------------------------------------------------------------------------------------------*/
+	public static String submitBtn=".//*[@id='btncreate']/button";
+	public static String clearBtn=".//*[@id='btnClr']";
+	
+}
