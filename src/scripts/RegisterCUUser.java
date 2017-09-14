d45adec0b5848cbaa7987c4930f5a8f57a109dfc
ArchitaS <ArchitaS@RSPC1195.rssoftware.com>
2017-09-14 16:50:27 +0530

    uservalidation

--- src/scripts/RegisterCUUser.java
+++ src/scripts/RegisterCUUser.java
@@ -4,6 +4,7 @@ import java.awt.AWTException;
 import java.awt.HeadlessException;
 import java.io.IOException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -17,9 +18,9 @@ import objectRepository.RegisterUser;
 public class RegisterCUUser extends Driver{
 	
 	
+	//**User ID Duplicacy Check **//
 	
-	
-//	@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	public static void DuplicacyChk(String[] data,String[] assertion) 
 	
 	{
@@ -44,6 +45,9 @@ public class RegisterCUUser extends Driver{
 			Thread.sleep(2000);
 			driver.findElement(By.id(RegisterUser.loginId)).sendKeys(data[0]);
 			System.out.println("Value Inputed");
+			
+					
+			
 			Thread.sleep(2000);
 			driver.findElement(By.id(RegisterUser.checkAvailibilitybtn)).click();
 			System.out.println("Clicked");
@@ -67,10 +71,9 @@ public class RegisterCUUser extends Driver{
 				Result = DuplicacyChk;
 				Thread.sleep(2000);
 			}
-			
-			
-			
-			
+						
+				
+			driver.findElement(By.xpath(Dasboard.home)).click();
 			FrameworkFunctions.reportGeneration(testSuiteName, testCaseName, Result);
 			FrameworkFunctions.screenShot(testCaseName);
 			
@@ -80,15 +83,151 @@ public class RegisterCUUser extends Driver{
 			
 		}
 			
-		if(DuplicacyChk.contains("Duplicate"))
+	}	
+	//**Emp ID Duplicacy Check **//
+		
+		public static void DuplicacyChkEmp(String[] data,String[] assertion) 
+		
 		{
-			System.out.println("Duplicate CHk Failed");
-		}
-		else if(DuplicacyChk.equalsIgnoreCase("available "))
-		{
-			System.out.println("Duplicate CHk Passed");
-		}
+			System.out.println("Register User Emp ID Duplicacy Check");
+			//String DuplicacyChk = "";
+			try{
+				
+				testSuiteName="RegisterCUUser";
+				testCaseName="DuplicacyChkEmp";
+				
+				String titel=driver.getTitle();
+				System.out.println("tite"+titel);
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(Dasboard.userMgmt)).click();
+				System.out.println("User Mgmt Clicked");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(Dasboard.createUser)).click();
+				System.out.println("Crt Usr Clicked");
+				Thread.sleep(2000);
+				driver.findElement(By.id(RegisterUser.userTypeCu)).click();
+				System.out.println("BBCU rd Button Clicked");
+				Thread.sleep(2000);
 			
+				driver.findElement(By.id(RegisterUser.empId)).sendKeys(data[0]);
+				driver.findElement(By.id(RegisterUser.empId)).sendKeys(Keys.TAB);
+				System.out.println("Value Inputed");						
+				Thread.sleep(2000);
+													
+				String ChkValueEmp = driver.findElement(By.xpath(RegisterUser.empIderr)).getText();
+				System.out.println("Value is : "+ChkValueEmp);
+				String ExpectedValueEmp = "Another user with the same employee id exists in the system.";
+				String ChkValueEmpDup = "";
+				if (ChkValueEmp.equals(ExpectedValueEmp))
+				{
+					driver.findElement(By.xpath(Dasboard.userMgmt)).click();
+					ChkValueEmpDup = driver.findElement(By.xpath(RegisterUser.empIderr)).getText();
+					System.out.println("Empl ID negative:" + ChkValueEmpDup);
+					Result = ChkValueEmpDup;
+					Thread.sleep(2000);
+				}
+				else
+				{
+					ChkValueEmpDup = driver.findElement(By.xpath(RegisterUser.empIdsucc)).getText();
+					System.out.println("Empl ID positive:" + ChkValueEmpDup);
+					Result = ChkValueEmpDup;
+					Thread.sleep(2000);
+				}
+				
+	
+				driver.findElement(By.xpath(Dasboard.home)).click();
+				FrameworkFunctions.reportGeneration(testSuiteName, testCaseName, Result);
+				FrameworkFunctions.screenShot(testCaseName);
+				
+			}catch(Exception e){
+				
+				System.out.println(e);
+				
+			}
+		
+	
+					
+			//Assert and write Report to File
+			
+			/** if(titel.equalsIgnoreCase("Dashboard"))
+			{
+				Result="Pass";
+				
+			}else{
+				
+				Result="Fail";
+				
+			}**/
+			
+					
+			
+			
+			
+		
+		}
+		
+		
+	public static void DuplicacyChkEmail(String[] data,String[] assertion) 
+		
+		{
+			System.out.println("Register User Email ID Duplicacy Check");
+			//String DuplicacyChk = "";
+			try{
+				
+				testSuiteName="RegisterCUUser";
+				testCaseName="DuplicacyChkEmail";
+				
+				String titel=driver.getTitle();
+				System.out.println("tite"+titel);
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(Dasboard.userMgmt)).click();
+				System.out.println("User Mgmt Clicked");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(Dasboard.createUser)).click();
+				System.out.println("Crt Usr Clicked");
+				Thread.sleep(2000);
+				driver.findElement(By.id(RegisterUser.userTypeCu)).click();
+				System.out.println("BBCU rd Button Clicked");
+				Thread.sleep(2000);
+			
+				driver.findElement(By.id(RegisterUser.emailId)).sendKeys(data[0]);
+				driver.findElement(By.id(RegisterUser.emailId)).sendKeys(Keys.TAB);
+				System.out.println("Value Inputed");						
+				Thread.sleep(2000);
+													
+				String ChkValueEmail = driver.findElement(By.xpath(RegisterUser.empIderr)).getText();
+				System.out.println("Value is : "+ChkValueEmail);
+				String ExpectedValueEmp = "Another user with the same email id exists in the system.";
+				String ChkValueEmailDup = "";
+				if (ChkValueEmail.equals(ChkValueEmailDup))
+				{
+					driver.findElement(By.xpath(Dasboard.userMgmt)).click();
+					ChkValueEmailDup = driver.findElement(By.xpath(RegisterUser.emailiderr)).getText();
+					System.out.println("Empl ID negative:" + ExpectedValueEmp);
+					Result = ChkValueEmailDup;
+					Thread.sleep(2000);
+				}
+				else
+				{
+					ChkValueEmailDup = driver.findElement(By.xpath(RegisterUser.emailidavail)).getText();
+					System.out.println("Empl ID positive:" + ExpectedValueEmp);
+					Result = ChkValueEmailDup;
+					Thread.sleep(2000);
+				}
+				
+	
+				driver.findElement(By.xpath(Dasboard.home)).click();
+				FrameworkFunctions.reportGeneration(testSuiteName, testCaseName, Result);
+				FrameworkFunctions.screenShot(testCaseName);
+				
+			}catch(Exception e){
+				
+				System.out.println(e);
+				
+			}
+		
+	
+					
 			//Assert and write Report to File
 			
 			/** if(titel.equalsIgnoreCase("Dashboard"))
